7ca832a5a44c585fa75a47c5cdfb977589097fd1
Prernaswarna <prernaswarnakumar@gmail.com>
2020-06-01 09:51:13 +0530

    trying oauth url

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
    componentDidMount() {
     axios
-     .get("http://localhost:8000/user")
+     .get("https://internet.channeli.in/oauth/authorise/?client_id=Z0D8JmQtXjXJxp63bsnt2HEGvWuw7sSRD19oZ4FO&redirect_url=http://127.0.0.1:8000/user/confirm&state=reachedpage")
       .then(response => {
         if (response.status > 400) {
           return this.setState(() => {
@@ -24,6 +24,7 @@ class App extends Component {
         }
         return response.json();
       })
+      
       .then(data => {
         this.setState(() => {
           return {
